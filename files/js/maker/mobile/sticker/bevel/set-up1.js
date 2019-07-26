149967f306e6530d72d329bf5650b79a6c8c80f4
lannister34 <Misha.Sokovets@yandex.ru>
2019-07-26 09:46:17 +0300

    da

--- files/js/maker/mobile/sticker/bevel/set-up1.js
+++ files/js/maker/mobile/sticker/bevel/set-up1.js
@@ -295,18 +295,13 @@
 
     /* ------------------------ CSS ------------------------ */
     var css_rules = {
-        "html, body" : {
-          "padding" : "0",
-          "margin" : "0",
-          "height" : "100%",
-          "width" : "100%"
-        },
-
         ".—prx—size" : {
           "opacity" : "0",
-          "position" : "fixed",
-          "height" : "100%",
-          "width" : "100%",
+          "position" : "absolute",
+          "left" : "0",
+          "right" : "0",
+          "top" : "0",
+          "bottom" : "0",
           "z-index" : "-1",
         },
 
